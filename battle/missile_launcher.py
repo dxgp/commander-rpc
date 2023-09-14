2ccaaa9861388491342ddd5ac9ef5cc3a4bea96b
Gunjan Payal <gunjanpayal@Gunjans-MacBook-Pro.local>
2023-09-14 15:04:32 +0530

    changed IPs for test

--- battle/missile_launcher.py
+++ battle/missile_launcher.py
@@ -3,9 +3,10 @@ from messages_pb2 import missile_details
 from messages_pb2_grpc import DefenseNotificationStub
 import random
 
-channel = grpc.insecure_channel("localhost:50000")
+channel = grpc.insecure_channel("172.17.85.111:50000")
 stub = DefenseNotificationStub(channel)
 x = random.randint(0, 9)
 y = random.randint(0, 9)
 request = missile_details(missile_type="m1", x=x, y=y, t=1)
+print("MISSILE LAUNCHED...")
 stub.launch_missile(request)
