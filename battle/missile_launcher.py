81952511f6e30894e3368f09e3ef4592bb1a4e4f
kakashiisawesome <shrinivaschowdhary97@gmail.com>
2023-09-28 21:26:50 +0530

    Randomize missile type before launch

--- battle/missile_launcher.py
+++ battle/missile_launcher.py
@@ -2,7 +2,7 @@ import grpc
 from messages_pb2 import missile_details
 from messages_pb2_grpc import DefenseNotificationStub
 import random
-from constants import MissileType,BoardEdges,t,T,DEFENSE_NOTIFICATION_IP
+from constants import MissileType, BoardEdges, t, T, DEFENSE_NOTIFICATION_IP
 import time
 
 
@@ -13,11 +13,15 @@ stub = DefenseNotificationStub(channel)
 
 starttime = time.time()
 local_t = 0
-while (local_t<=T):
+while local_t <= T:
     local_t += t
     x = random.randint(0, BoardEdges.RIGHT_X)
     y = random.randint(0, BoardEdges.BOTTOM_Y)
-    missile_type = MissileType.M3
+
+    missile_types = [MissileType.M1, MissileType.M2, MissileType.M3, MissileType.M4]
+    missile_random_index = random.randint(0, len(missile_types) - 1)
+
+    missile_type = missile_types[missile_random_index]
     request = missile_details(missile_type=missile_type, x=x, y=y, t=t)
     print("MISSILE LAUNCHED...")
     stub.launch_missile(request)
